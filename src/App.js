c0d2f75dc7bfd1a9e5c4362fce883dd78899b856
Seojin Kim <skim27@wustl.edu>
2021-04-17 15:50:48 -0500

    added basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import School from './School'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [schools, setSchools] = useState([])
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  const getWindowWidth = () => {
+    const width = window.innerWidth
+    setWindowWidth(width)
+  }
+
+  const getSchools = async () => {
+    const response = await (await fetch(`https://rmdservice.herokuapp.com/api/schools`)).json()
+    const data = await response.results
+    setSchools(data)
+
+  }
+
+  useEffect(()=>{
+    getWindowWidth()
+    window.addEventListener("resize", getWindowWidth)
+    return () => {
+      window.removeEventListener("resize", getWindowWidth)
+    }
+  },[])
+
+  useEffect(()=>{
+    getSchools()
+  },[])
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button type="submit" className="search-button">submit</button>
+      </form>
+      {schools.map(school => (
+        <School 
+          name={school.name} 
+          url={school.url}
+          windowSize={windowWidth}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/School.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './school.module.css'
+
+function School(props) {
+
+    return(
+        <a className={style.School} onClick={'/'+props.url}>
+            <h1>{props.name}</h1>
+        </a>
+    )
+}
+
+export default School
